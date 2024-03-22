a52e3d4774acf6278d97b0ca1159d6c9637c6637
Meshwa123 <101788545+Meshwa123@users.noreply.github.com>
2024-03-22 19:58:58 +0530

    Delete routes directory

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser, forgotPassword } = require('../controllers/authController');
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/forgot-password', forgotPassword);
-
-module.exports = router;
